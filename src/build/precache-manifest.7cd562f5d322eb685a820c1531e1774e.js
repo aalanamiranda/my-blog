4e4d45eba62642b61d854772cd8d61014f64c144
aalanamiranda <61668538+aalanamiranda@users.noreply.github.com>
2020-06-05 11:32:07 -0400

    First commit

---
+++ src/build/precache-manifest.7cd562f5d322eb685a820c1531e1774e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f3b0a768d63367b15b4",
+    "url": "/static/css/main.546a9750.chunk.css"
+  },
+  {
+    "revision": "6f3b0a768d63367b15b4",
+    "url": "/static/js/main.9527fdda.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e135aa781dc249d61fe",
+    "url": "/static/js/2.67d4839a.chunk.js"
+  },
+  {
+    "revision": "503a7f81e6fcfc059acef585f15f2b7c",
+    "url": "/index.html"
+  }
+];
